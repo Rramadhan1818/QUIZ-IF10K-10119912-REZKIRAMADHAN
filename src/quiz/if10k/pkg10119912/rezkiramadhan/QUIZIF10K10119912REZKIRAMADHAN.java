9761b2a66003ca01b605914519b0352715b0a62c
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-27 20:33:10 +0700

    first add

---
+++ src/quiz/if10k/pkg10119912/rezkiramadhan/QUIZIF10K10119912REZKIRAMADHAN.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if10k.pkg10119912.rezkiramadhan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public class QUIZIF10K10119912REZKIRAMADHAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
